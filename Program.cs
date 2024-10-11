ff0e606ede03f9490b0b0bb75b3b40d3611b5beb
BRicsi <barnusrichi@gmail.com>
2024-10-11 11:44:23 +0200

    Empty project is created.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace napszak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentTime = DateTime.Now;
+
+            if (currentTime.Hour >= 5 && currentTime.Hour < 12)
+            {
+                Console.WriteLine("Jó reggelt!");
+            }
+            else if (currentTime.Hour >= 12 && currentTime.Hour < 18)
+            {
+                Console.WriteLine("Jó napot!");
+            }
+            else if (currentTime.Hour >= 18 && currentTime.Hour < 22)
+            {
+                Console.WriteLine("Jó estét!");
+            }
+            else
+            {
+                Console.WriteLine("Jó éjszakát!");
+            }
+        }
+    }
+}
